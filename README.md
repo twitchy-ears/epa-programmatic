65393bb6d4c8f77a509e01da87453ebc2578fe12
twitchy-ears <45134107+twitchy-ears@users.noreply.github.com>
2025-05-14 12:17:10 +0100

    Changed over to actually querying the internals of EPG instead of buffer parsing EPA.  Added more flexible data structures and filtering

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ that would let me put in a name, or an email address or some other
 criteria, get those keys, and then use those key-ids with plstore
 and similar.
 
+I initially parsed the epa buffered output (those functions are
+commented out later down the file) but dug into them a little
+afterwards and switched to querying the epg library directly
+
 For example very very simply and probably dangerously: 
 
 ```
@@ -24,14 +28,29 @@ For example very very simply and probably dangerously:
           (add-to-list 'plstore-encrypt-to curr)))))
 ```
 
-If you wanted to get the key-id and user data from every key (not just secret keys) that had @foo.com in its user data (be that in an email or a name, beware regexps, etc.etc.) then something like this:
+If you wanted to look for all keys related to `@foo.com` emails (yes,
+beware regexps) with both ultimate or full trust, and retrieve the
+key-id, user-id-string (usually "Firstname Surname
+<email@domain.tld>"), and the trust character (u(ltimate), f(ull), o
+(unknown), q (undefined), n(ever), m(arginal), - (none)) then
+something like this:
+
 
 ```
-;; Filter on data field 2 (user-data), for the regexp given,
-;; don't limit ourselves to secret keys, and return fields 1 and 2
-;; (keyid, user-data) 
-(epa-programmatic-get-keys-filtered 2 ".*@foo\.com.*" nil '(1 2))
+(epa-programmatic-get-data-filtered
+           '((:user-id-string "@foo.com")
+             (:trust-char "[uf]"))
+           nil
+           '(:key-id :user-id-string :trust-char))"
 ```
+
+What functions are available?
+
+* `epa-programmatic-get-data`: Retrieves all the data from either public or secret keyrings, has some basic filtering from the functions it calls.
+* `epa-programmatic-get-data-filtered`: Retrieves flexible fields from keys based on flexible filtering
+* `epa-programmatic-ultimate-trusted-keyids`: Retrieves a list of key-ids of all keys with ultimate trust and validity from the secret keyring.
+* `epa-programmatic-buffer-get-keys`: Parses the epa output buffer to get its data, deprecated.
+* `epa-programmatic-buffer-get-keys-filtered`: Allows some filtering of data and results from epa buffers, deprecated.
 
 # BUGS
 
@@ -39,4 +58,4 @@ Almost certainly
 
 # Is there a better way of doing this?
 
-Almost certainly but messing around inside epa feels like more work than a quick buffer parser.
+Almost certainly, take a look inside `epa-programmatic-get-data` and you'll see what we're parsing, you can probably build something more specific from the guts of `epg-*`
--- epa-programmatic.el
+++ epa-programmatic.el
@@ -1,4 +1,4 @@
-;;; epa-programmatic.el --- Querying Emacs epa / EasyPG Assistant GnuPG library of keys programmatically -*- lexical-binding: t -*-
+;;; epa-programmatic.el --- Querying Emacs epg / epa / EasyPG Assistant GnuPG library of keys programmatically -*- lexical-binding: t -*-
 
 ;; Copyright 2025 - Twitchy Ears
 
@@ -28,6 +28,7 @@
 ;;; History
 ;;
 ;; 2025-05-13 - initial version
+;; 2025-05-14 - changed functions around, now a thin shim over EPG
 
 ;;; Commentary:
 
@@ -38,6 +39,12 @@
 ;; that would let me put in a name, or an email address or some other
 ;; criteria, get those keys, and then use those key-ids with plstore
 ;; and similar.
+;;
+;; I initially parsed the epa buffered output (those functions are
+;; commented out later down the file) but dug into them a little
+;; afterwards and switched to querying the epg library directly.
+;;
+;; The original epa buffer querying functions are still available if wanted.
 ;; 
 ;; (use-package epa-programmatic)
 ;; 
@@ -49,10 +56,236 @@
 ;;     (if (length> ukeys 0)
 ;;         (dolist (curr ukeys)
 ;;           (add-to-list 'plstore-encrypt-to curr)))))
+;;
+;; Example, getting all keys related to @foo.com with ultimate or full
+;; trust in the keyring, and retrieving a list of key-id,
+;; user-id-string, and the trust character (u, f, m, o, q, -, etc)
+;;
+;; (epa-programmatic-get-data-filtered
+;;           '((:user-id-string "@foo.com")
+;;             (:trust-char "[uf]"))
+;;           nil
+;;           '(:key-id :user-id-string :trust-char))"
 
 (require 'epa)
+(require 'epg)
 
-(defun epa-programmatic-get-keys (&optional secret)
+(defun epa-programmatic-get-data (&optional filter secret)
+  "Processes through the GnuPG/epa/epg data directly, returns a list of
+alists in the form of: 
+
+( ( (:validity-string . validity-string)
+    (:validity-char . validity-char)
+    (:user-id-string . user-id-string)
+    (:owner-trust . owner-trust)
+    (:trust-char . trust-char)
+    (:key-id . key-id) )
+
+  ( (:validity-string . validity-string)
+    (:validity-char . validity-char)
+    (:user-id-string . user-id-string)
+    (:owner-trust . owner-trust)
+    (:trust-char . trust-char)
+    (:key-id . key-id) )
+
+  ... )
+
+These values can then be accessed with something like:
+
+(dolist (entry (epa-programmatic-get-data))
+  (message \"key-id '%s'\" (alist-get :key-id entry)))
+
+Where validity-string and validity-char are both drawn from
+`epg-key-validity-alist' (but are given as strings rather than a
+(char . symbol) pairing for ease of looking at later)
+
+The user-id-string is from the key and is often
+\"Firstname Surname <email@domain.tld>\" formatted
+
+The owner-trust string is a description of how trusted the key is as per
+GnuPG standards so should be one of: ultimate, full, unknown, undefined,
+never, marginal.
+
+The trust-char is owner-trust looked up from epg-key-validity-alist but as
+a string, likely to be u(ltimate), f(ull), o (unknown), q (undefined),
+n(ever), m(arginal), - (none).
+
+The key-id is the long ID of the subkey that has the 'sign and 'certify
+qualities and is hence likely the public key you're interested in.
+
+If there is a filter then only keys matching it (I think in user-id-string)
+will be returned, if secret is t then only the secret keyring will be searched"
+
+  (interactive)
+  (let* ((context (epg-make-context epa-protocol))
+         (data (epg-list-keys context filter secret))
+         (acc '()))
+    
+    ;; Run through all the keys found
+    (dolist (dat data)
+      ;; (message "DAT '%s'" dat)
+      (let* ((uid-list (epg-key-user-id-list dat))
+             (validity (epg-user-id-validity (car uid-list)))
+             (subkey-list (epg-key-sub-key-list dat))
+             (ret '()))
+        
+        ;; Process through the associated UIDs
+        ;; (dolist (uid uid-list)
+        ;; (message "uid validity '%s' user '%s'"
+        ;;          (epg-user-id-validity uid)
+        ;;          (epg-user-id-string uid))
+        ;; (push (epg-user-id-validity uid) ret)
+        ;; (push (epg-user-id-string uid) ret))
+
+        ;; Just grab the first identity
+        (push (cons :validity (symbol-name validity)) ret)
+        (push (cons :validity-char
+                    (char-to-string
+                     (car (rassq validity
+                                 epg-key-validity-alist))))
+              ret)
+        (push (cons :user-id-string (epg-user-id-string (car uid-list))) ret)
+        
+        ;; Owner trust
+        ;; (message "owner-trust '%s'" (epg-key-owner-trust dat))
+        (push (cons :owner-trust (symbol-name (epg-key-owner-trust dat))) ret)
+        (push (cons :trust-char
+                    (char-to-string
+                     (car (rassq (epg-key-owner-trust dat)
+                                 epg-key-validity-alist))))
+              ret)
+        
+        ;; Pick the most interesting subkey
+        (dolist (skey subkey-list)
+          ;; (message "sub-key-id '%s' -> '%s'" (epg-sub-key-capability skey) (epg-sub-key-id skey))
+          (if (and (member 'sign (epg-sub-key-capability skey))
+                   (member 'certify (epg-sub-key-capability skey)))
+              (push (cons :key-id (epg-sub-key-id skey)) ret)))
+        
+        (push (reverse ret) acc)))
+
+    (when (called-interactively-p 'any)
+      (message "%s" acc))
+    
+    acc))
+
+
+
+
+(defun epa-programmatic-get-data-filtered (filter &optional secret return-fields)
+
+  "Uses the `epa-programmatic-get-data' function but allows ease of filtering
+its output for specific values, and also filtering what values are returned.
+
+The `filter' is a list of lists, the car of which should be a :keyword
+and the cdr of which should be a regexp to test against this.  If all
+of these match then the key is returned.  If a single one fails then the
+whole entry is rejected.
+
+`field' should be a :keyword from the data structure of
+epa-programmatic-get-data and `filter' is a regex that will be tested
+against this field.  If both are nil then all results will be returned.
+
+The optional argument `secret' should be nil for all keys on the keyring
+ and t for just the secret keys.
+
+The `return-fields' should be a list of :keyword entries from the data
+structure of epa-programmatic-get-data, if nil it defaults to '(:key-id)
+
+For example: If you wanted to search for all keys with \@foo.com in the
+user ID with ultimate or full trust, and get the key-id, user-id-string,
+and trust level then you'd want something like:
+
+(epa-programmatic-get-data-filtered
+           '((:user-id-string \"@foo.com\")
+             (:trust-char \"[uf]\"))
+           nil
+           '(:key-id :user-id-string :trust-char))"
+  
+  (interactive)
+
+  (if (not return-fields)
+      (setq return-fields '(:key-id)))
+
+  ;; Get unfiltered data because we do more complex checking later
+  (let ((data (epa-programmatic-get-data nil secret))
+        (return '()))
+    
+    (dolist (entry data)
+      (let ((matched nil)
+            (result '()))
+
+        ;; Work out if we're returning this entry or not
+        (if filter
+            
+            ;; Actually filter, if any test fails then don't return the data
+            (let ((failed nil))
+              
+              (dolist (filt filter)
+                ;; (message "Processing filter '%s' '%s' '%s'" filt (car filt) (nth 1 filt))
+                (let* ((field (car filt))
+                       (filter-regexp (nth 1 filt))
+                       (field-data (alist-get field entry)))
+                  
+                  (if (not (string-match-p filter-regexp field-data))
+                      (setq failed t))))
+              
+              (if (not failed)
+                  (setq matched t)))
+
+          ;; No filter?  Just return
+          (setq matched t))
+
+        ;; Extract the wanted fields
+        (when matched
+          (dolist (key return-fields)
+            (let ((val (alist-get key entry)))
+              (push val result)))
+          
+          ;; Accumulate onto returnable results, reversing the order
+          ;; because everything was pushed above
+          (push (reverse result) return))))
+
+    ;; (reverse return)
+    (when (called-interactively-p 'any)
+      (message "%s" return))
+
+    return))
+        
+                
+
+
+(defun epa-programmatic-ultimate-trusted-keyids ()
+  "Returns a list of strings, each of which is the keyid of a GPG key with
+ultimate trust and validity from the GnuPG keyring.
+
+Returns an empty list if there are none."
+  (interactive)
+  (let ((raw (epa-programmatic-get-data-filtered
+              '((:validity "^ultimate$")
+                (:owner-trust "^ultimate$"))
+              t
+              '(:key-id)))
+        (results '()))
+
+    ;; Extract the keyids from the list above as each will be on a
+    ;; results sublist
+    (dolist (curr raw)
+      (push (car curr) results))
+
+    ;; Reverse because we were pushing
+    (setq results (reverse results))
+    
+    (when (called-interactively-p 'any)
+      (message "%s" results))
+    results))
+
+
+
+;; Some old code that parses the epa buffers instead of querying epg directly.
+
+
+(defun epa-programmatic-buffer-get-keys (&optional secret)
   "Returns a list of lists of all the keys that epa knows about.
 Each list is three strings, with the first being qualities known by EPA
 (see `epa-list-keys' for examples of these but e is expired, u is ultimate
@@ -96,11 +329,13 @@ If the `secret' argument is given checks the data from epa-list-secret-keys"
         (message "%s" found-keys))
       found-keys)))
 
-(defun epa-programmatic-get-keys-filtered (field filter &optional secret return-fields)
+
+
+(defun epa-programmatic-buffer-get-keys-filtered (field filter &optional secret return-fields)
 
   "Filters the keys known to epa (EasyPG Assistant) and returns keyids based on
-the criteria given.  Retrieves data with `epa-programmatic-get-keys' which
-gives a list of all keys known in the format
+the criteria given.  Retrieves data with `epa-programmatic-buffer-get-keys'
+which gives a list of all keys known in the format
 
 '( (descriptive-character key-id user-details) ...)
 
@@ -119,7 +354,7 @@ Returns an empty list if there are none."
   (if (not return-fields)
       (setq return-fields '(0 1 2)))
   
-  (let ((epa-data (epa-programmatic-get-keys secret))
+  (let ((epa-data (epa-programmatic-buffer-get-keys secret))
         (found-keydata '()))
     
     (dolist (curr epa-data)
@@ -158,24 +393,10 @@ Returns an empty list if there are none."
       (message "%s" found-keydata))
     found-keydata))
 
-(defun epa-programmatic-ultimate-trusted-keyids ()
-  "Returns a list of strings, each of which is the keyid of a GPG key with ultimate trust
-via epa/EasyPG Assistant.  Returns an empty list if there are none."
-  (interactive)
-  (let ((raw (epa-programmatic-get-keys-filtered 0 "^u$" t 1))
-        (results '()))
 
-    ;; Extract the keyids from the list above as each will be on a
-    ;; results sublist
-    (dolist (curr raw)
-      (push (car curr) results))
 
-    ;; Reverse because we were pushing
-    (setq results (reverse results))
-    
-    (when (called-interactively-p 'any)
-      (message "%s" results))
-    results))
+
+
 
 
 (provide 'epa-programmatic)
